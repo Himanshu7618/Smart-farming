18c907d374c1912cc2d0237e1241921616a5edb4
Himanshu7618 <hc5589414@gmail.com>
2026-06-04 17:04:31 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const app = express();
 // middleware
 app.use(cors(
 {
-  origin:["http://localhost:5173", "http://localhost:5174"],
+  origin:["http://localhost:5173", "http://localhost:5174", "https://smart-farming-three.vercel.app/"],
   credentials: true
 }));
 app.use(express.json());
